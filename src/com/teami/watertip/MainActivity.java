3c0945ee0b54100e0a2fef9300a85557ccb0cd4d
huangtao800 <huangtao800@gmail.com>
2014-01-15 00:55:55 +0800

    modify

--- src/com/teami/watertip/MainActivity.java
+++ src/com/teami/watertip/MainActivity.java
@@ -3,7 +3,6 @@ package com.teami.watertip;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
-import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
