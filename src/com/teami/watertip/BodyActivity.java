590d637ad198bc53f44b15bafae4b71aada0ca2b
huangtao800 <huangtao800@gmail.com>
2014-01-15 14:07:41 +0800

    add century gothic font

--- src/com/teami/watertip/BodyActivity.java
+++ src/com/teami/watertip/BodyActivity.java
@@ -1,15 +1,22 @@
 package com.teami.watertip;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.File;
 
-public class BodyActivity extends Activity{
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
+
+public class BodyActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.body_layout);	
+		setContentView(R.layout.body_layout);
+		TextView titleView = (TextView) findViewById(R.id.app_title);
+		Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/centurygothic.ttf");
+		titleView.setTypeface(typeface);
 	}
-	
-	
+
 }
