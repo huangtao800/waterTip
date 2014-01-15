13f19a1cf1ccca568da141182ab46f7716e6c5a8
huangtao800 <huangtao800@gmail.com>
2014-01-15 15:30:20 +0800

    add Date

--- src/com/teami/watertip/BodyActivity.java
+++ src/com/teami/watertip/BodyActivity.java
@@ -1,11 +1,10 @@
 package com.teami.watertip;
 
-import java.io.File;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Environment;
 import android.widget.TextView;
 
 public class BodyActivity extends Activity {
@@ -15,8 +14,17 @@ public class BodyActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.body_layout);
 		TextView titleView = (TextView) findViewById(R.id.app_title);
-		Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/centurygothic.ttf");
+		Typeface typeface = Typeface.createFromAsset(getAssets(),
+				"fonts/centurygothic.ttf");
 		titleView.setTypeface(typeface);
+		
+		//…Ë÷√»’∆⁄
+		Calendar instance = Calendar.getInstance();
+		TextView dateView = (TextView) findViewById(R.id.date_textview);
+		dateView.setText(instance.get(Calendar.YEAR)
+				+ "-"
+				+ (instance.get((Calendar.MONTH) + 1) + "-" + instance
+						.get(Calendar.DAY_OF_MONTH)));
+		dateView.setTypeface(typeface);
 	}
-
 }
