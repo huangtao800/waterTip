42839775b3804ff4349ed82880f14bca2eb48887
huangtao800 <huangtao800@gmail.com>
2014-01-15 10:07:34 +0800

    add BodyActivity

--- src/com/teami/watertip/MainActivity.java
+++ src/com/teami/watertip/MainActivity.java
@@ -2,8 +2,10 @@ package com.teami.watertip;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
@@ -20,13 +22,41 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onPause(){
 		super.onPause();
+		//stop animating
 		ImageView logoView=(ImageView)findViewById(R.id.ImageViewLogo);
 		logoView.clearAnimation();
+	}
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		startAnimating();
 	}
 
 	private void startAnimating(){
 		View imageView=findViewById(R.id.ImageViewLogo);
 		Animation fadeAnimation=AnimationUtils.loadAnimation(this, R.anim.fadein);
 		imageView.startAnimation(fadeAnimation);
+		fadeAnimation.setAnimationListener(new AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onAnimationRepeat(Animation arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onAnimationEnd(Animation arg0) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent(MainActivity.this,BodyActivity.class));
+				MainActivity.this.finish();
+			}
+		});
 	}
 }
