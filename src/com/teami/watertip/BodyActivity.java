1fe90927da0fb2767f2763f3fac3a39e0f5374d1
lee <lee19930131@gmail.com>
2014-01-15 11:39:22 +0800

    Modify

--- src/com/teami/watertip/BodyActivity.java
+++ src/com/teami/watertip/BodyActivity.java
@@ -1,7 +1,15 @@
 package com.teami.watertip;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class BodyActivity extends Activity{
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.body_layout);	
+	}
+	
+	
 }
