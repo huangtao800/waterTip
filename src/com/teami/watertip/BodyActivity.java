c70a1bfc413855946a38dcef5e49d84f6115f762
huangtao800 <huangtao800@gmail.com>
2014-01-16 21:19:27 +0800

    将设置字体、设置日期、设置剩余杯数从onCreate()方法中抽离出来

--- src/com/teami/watertip/BodyActivity.java
+++ src/com/teami/watertip/BodyActivity.java
@@ -9,32 +9,56 @@ import android.widget.TextView;
 
 public class BodyActivity extends Activity {
 
+	private Typeface typeface = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.body_layout);
-		TextView titleView = (TextView) findViewById(R.id.app_title);
-		Typeface typeface = Typeface.createFromAsset(getAssets(),
+		typeface = Typeface.createFromAsset(getAssets(),
 				"fonts/centurygothic.ttf");
-		titleView.setTypeface(typeface);
-		
-		
-		//设置日期
+		setDate();
+		setNumberLeft();
+		setFont();
+	}
+
+	/**
+	 * 设置日期
+	 */
+	private void setDate() {
 		Calendar instance = Calendar.getInstance();
 		TextView dateView = (TextView) findViewById(R.id.date_textview);
 		dateView.setText(instance.get(Calendar.YEAR)
 				+ "/"
-				+ ((instance.get(Calendar.MONTH)+1) + "/" + instance
+				+ ((instance.get(Calendar.MONTH) + 1) + "/" + instance
 						.get(Calendar.DAY_OF_MONTH)));
-		dateView.setTypeface(typeface);
-		
-		TextView countView=(TextView) findViewById(R.id.count_textview);
-		countView.setTypeface(typeface);
-		
-		//设置剩余杯数
-		TextView leftNumberView=(TextView)findViewById(R.id.left_number_textview);
-		leftNumberView.setTypeface(typeface);
+	}
+
+	/**
+	 * 设置剩余杯数
+	 */
+	private void setNumberLeft() {
+		TextView leftNumberView = (TextView) findViewById(R.id.left_number_textview);
 		leftNumberView.setText("3 to go!");
-		
+	}
+
+	/**
+	 * 设置字体
+	 */
+	private void setFont() {
+		TextView countView = (TextView) findViewById(R.id.count_textview);
+		countView.setTypeface(typeface);
+
+		TextView titleView = (TextView) findViewById(R.id.app_title);
+		titleView.setTypeface(typeface);
+
+		TextView leftNumberView = (TextView) findViewById(R.id.left_number_textview);
+		leftNumberView.setTypeface(typeface);
+
+		TextView dateView = (TextView) findViewById(R.id.date_textview);
+		dateView.setTypeface(typeface);
+
+		TextView tipView = (TextView) findViewById(R.id.tip_textview);
+		tipView.setTypeface(typeface);
 	}
 }
