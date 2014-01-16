970f7bd3c4d2382f584e61d4361a8ea93e3a0e9b
huangtao800 <huangtao800@gmail.com>
2014-01-16 17:20:52 +0800

    修改日期表示

--- src/com/teami/watertip/BodyActivity.java
+++ src/com/teami/watertip/BodyActivity.java
@@ -16,25 +16,25 @@ public class BodyActivity extends Activity {
 		TextView titleView = (TextView) findViewById(R.id.app_title);
 		Typeface typeface = Typeface.createFromAsset(getAssets(),
 				"fonts/centurygothic.ttf");
-//		titleView.setTypeface(typeface);
+		titleView.setTypeface(typeface);
 		
 		
 		//设置日期
 		Calendar instance = Calendar.getInstance();
 		TextView dateView = (TextView) findViewById(R.id.date_textview);
 		dateView.setText(instance.get(Calendar.YEAR)
-				+ "-"
-				+ ((instance.get(Calendar.MONTH)+1) + "-" + instance
+				+ "/"
+				+ ((instance.get(Calendar.MONTH)+1) + "/" + instance
 						.get(Calendar.DAY_OF_MONTH)));
 		dateView.setTypeface(typeface);
 		
 		TextView countView=(TextView) findViewById(R.id.count_textview);
-//		countView.setTypeface(typeface);
+		countView.setTypeface(typeface);
 		
 		//设置剩余杯数
 		TextView leftNumberView=(TextView)findViewById(R.id.left_number_textview);
 		leftNumberView.setTypeface(typeface);
-//		leftNumberView.setText("3 to go!");
+		leftNumberView.setText("3 to go!");
 		
 	}
 }
