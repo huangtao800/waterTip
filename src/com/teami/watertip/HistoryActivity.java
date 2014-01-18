600f3a84797450657cf01e69b4e74e4a5dd7b6dd
user <user@idea-PC>
2014-01-18 14:28:28 +0800

    history

--- src/com/teami/watertip/HistoryActivity.java
+++ src/com/teami/watertip/HistoryActivity.java
@@ -3,127 +3,133 @@ package com.teami.watertip;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.achartengine.ChartFactory;
 import org.achartengine.chart.PointStyle;
+import org.achartengine.renderer.SimpleSeriesRenderer;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 import org.xmlpull.v1.XmlPullParserException;
-
 import com.teami.model.HistoryModel;
-import android.app.Activity;
 import android.content.res.XmlResourceParser;
 import android.graphics.Color;
 import android.graphics.Paint.Align;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextSwitcher;
-import android.widget.TextView;
-import android.widget.ViewSwitcher.ViewFactory;
 
 public class HistoryActivity extends BaseActivity{
-	private String title = "喝水历史统计";
-	private double[] xContent;
-	private double[] yContent;
-	private double minXStart=0.5; private double maxXStart=7.5;
-	private double minYStart=0;   private double maxYStart=10;
-	
-	private final int xNumber=7;
-	private HistoryModel barCharModel;
-	
-	@Override
+	private HistoryModel historyModel ;
+    private String title = "";
+    //折线所表示的含义
+    private String lineInfoString = "";
+    private String dateString = "";
+    private String numOfWaterString = "";  
+    private ArrayList<String[]> list;
+	private double maxValue = 0; //数据的最大值
+	private int color;
+ 	
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        barCharModel=HistoryModel.getInstance();
-        //可以是多条折线混合，titles是对每条折线的说明
-        //当前情况下只有一天折线，表示每日喝水量
-        String[] titles = new String[] { title };
-        
-        //横坐标显示的内容,显示日期
-        getHistory();
-        List<double[]> x = new ArrayList<double[]>();
-        for (int i = 0; i < titles.length; i++) {
-            x.add(xContent);
-        }
-        
-        //纵坐标 图标上的数值，每个对应title中的一个
-        List<double[]> values = new ArrayList<double[]>();
-        for(int i=0;i<yContent.length;i++){
-        	if(yContent[i]>maxYStart){
-        		maxYStart = yContent[i];
-        	}
-        }
-        values.add(yContent);
-        
-        
-        //每条线的颜色
-        int[] colors = new int[] {getResources().getColor(R.color.font_color) };
-        //每个线上点的形状
-        PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE };
-        
-        //渲染器
-        XYMultipleSeriesRenderer renderer = barCharModel.buildRenderer(colors, styles);
-        int length = renderer.getSeriesRendererCount();
-        
-        //设置图上的点为实心 并且显示数值
-        for (int i = 0; i < length; i++) {
-            ((XYSeriesRenderer) renderer.getSeriesRendererAt(i)).setFillPoints(true);
-            ((XYSeriesRenderer)renderer.getSeriesRendererAt(i)).setDisplayChartValues(true);
-        }
-        
-        //图标上的简单属性，标题，横坐标，纵坐标，横坐标起始和终止位置，纵坐标起始和终止为位置
-        barCharModel.setChartSettings(renderer, "Average temperature", "Month", "Temperature", minXStart, maxXStart,
-        		minYStart, maxYStart, Color.LTGRAY, Color.LTGRAY);
-        
-        //横纵坐标显示多少个点，根据最大最小值自动分配
-        renderer.setXLabels(xNumber);
-        renderer.setYLabels((int)(maxYStart-minYStart+1));
-        
-        //是否显示网格
-        renderer.setShowGrid(true);
-        //设置刻度线与刻度线之间的相对位置
-        renderer.setXLabelsAlign(Align.RIGHT);
-        renderer.setYLabelsAlign(Align.RIGHT);
-
-        //是否显示放大缩小按钮
-        renderer.setZoomButtonsVisible(true);
-        //设置拖动时的最大最小值
-        renderer.setPanLimits(new double[] { -10, 20, minYStart-10, maxYStart+10 });
-        //设置放大缩小时的最大最小值
-        renderer.setZoomLimits(new double[] { -10, 20, minYStart-10, maxYStart+10 });
-
-        View view = ChartFactory.getLineChartView(this, barCharModel.buildDataset(titles, x, values), renderer);
-        view.setBackgroundColor(Color.WHITE);
-        setContentView(view);
-    }
-
-	private void getHistory(){
-		XmlResourceParser tipParser=getResources().getXml(R.xml.tips);
-		ArrayList<ArrayList<Double>> xy=new ArrayList<ArrayList<Double>>();
+		super.onCreate(savedInstanceState);
+		historyModel = HistoryModel.getInstance();
+		init(); //初始化组件
+		//表示目前就一条折线信息
+		String[] titles = new String[] {lineInfoString};
+		//List有几条数据
+		List<double[]> x = new ArrayList<double[]>();
+		//X轴所显示的数目 1,2,3,4,5.... 1条数据有几个点信息
+		double[] xInfo = new double[list.size()];
+		int count = 1;
+		for (int i = 0; i < list.size(); i++){
+			xInfo[i] = count;
+			count++;
+		}
+ 
+		//折线数量 1
+		for (int i = 0; i < titles.length; i++) {
+			x.add(xInfo);
+		}
+		//折线各个点的值
+		double[] xValue = new double[list.size()];
+		for (int i = 0; i < xValue.length; i++){
+			xValue[i] = Double.parseDouble((list.get(i))[1]);
+			//把数据最大值赋给maxValue
+			if (xValue[i] > maxValue){
+				maxValue = xValue[i];
+			}
+			}
+		maxValue = maxValue + (maxValue/8);
+ 
+		List<double[]> values = new ArrayList<double[]>();
+		values.add(xValue);
+		
+		color = getResources().getColor(R.color.font_color);
+		int[] colors = new int[] {color};//折线的颜色
+		PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE}; //折线的样式
+		XYMultipleSeriesRenderer renderer = historyModel.buildRenderer(colors, styles);
+		int length = renderer.getSeriesRendererCount();
+		for (int i = 0; i < length; i++) {
+			((XYSeriesRenderer) renderer.getSeriesRendererAt(i)).setFillPoints(true);
+		}
+		
+		//图标上的简单属性，标题，横坐标，纵坐标，横坐标起始和终止位置，纵坐标起始和终止为位置
+		setChartSettings(renderer, title, "", "", 0.5, 12.5, -10, 40, Color.DKGRAY, Color.DKGRAY);
+		renderer.setZoomButtonsVisible(true);
+		View view = ChartFactory.getLineChartView(this, historyModel.buildDataset(titles, x, values), renderer);
+		view.setBackgroundColor(Color.WHITE);
+		setContentView(view);
+	}
+ 
+    
+    private void init() throws IndexOutOfBoundsException{
+    	
+		XmlResourceParser recordParser=getResources().getXml(R.xml.record);
 		try {
-			xy=barCharModel.parseTipList(tipParser);
+			list=historyModel.parseRecordList(recordParser);
 		} catch (XmlPullParserException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		ArrayList<Double> xArrayList = new ArrayList<Double>();
-		xArrayList = xy.get(0);
-		xContent = new double[xArrayList.size()];
-		for(int i=0;i<xArrayList.size();i++){
-			xContent[i] = xArrayList.get(i);
-		}
-		
-		ArrayList<Double> yArrayList = new ArrayList<Double>();
-		yArrayList = xy.get(1);
-		yContent = new double[yArrayList.size()];
-		for(int i=0;i<yArrayList.size();i++){
-			yContent[i] = yArrayList.get(i);
-		}
+		title =  getResources().getString(R.string.historyTitle);
+        lineInfoString = getResources().getString(R.string.historyaLineInfo);
+        dateString = getResources().getString(R.string.date);
+        numOfWaterString = getResources().getString(R.string.numOfWater);
+
 		
 	}
-   
+    
+    public void setChartSettings(XYMultipleSeriesRenderer renderer, String title, String xTitle,
+			String yTitle, double xMin, double xMax, double yMin, double yMax, 
+			int axesColor, int labelsColor) {
+		renderer.setChartTitle(title);
+    	renderer.setYTitle(numOfWaterString);// 设置Y轴名称
+		renderer.setXTitle(dateString);
+		renderer.setXAxisMin(0.5);// 设置X轴的最小值为0.5
+		renderer.setXAxisMax(7.5);// 设置X轴的最大值为5
+		renderer.setYAxisMin(0);// 设置Y轴的最小值为0
+		renderer.setApplyBackgroundColor(true);
+		renderer.setBackgroundColor(Color.WHITE);
+		renderer.setMarginsColor(Color.WHITE);
+		renderer.setYAxisMax(maxValue);// 设置Y轴最大值
+		//设置显示对应位置上的数值
+//		renderer.setDisplayChartValues(true);
+		renderer.getSeriesRendererAt(0).setDisplayChartValues(true); 
+		// 设置x,y轴显示的排列,默认是 Align.CENTER
+        renderer.setXLabelsAlign(Align.CENTER);
+        renderer.setYLabelsAlign(Align.RIGHT);
+        // 设置坐标轴,轴的颜色
+//        renderer.setAxesColor(Color.DKGRAY);
+        // 显示网格
+        renderer.setShowGrid(true);
+        // 设置x,y轴上的刻度的颜色
+//        renderer.setLabelsColor(Color.DKGRAY);
+	    renderer.setXLabels(0);// 设置X轴显示的刻度标签的个数
+	    renderer.setBarSpacing(1);
+	    renderer.setXLabels(0);// 设置X轴显示的刻度标签的个数
+	    int tempNum = 1;
+	    for (String[] dataString : list){
+	    	renderer.addXTextLabel(tempNum,dataString[0]); 
+	    	tempNum++;
+	    }
+	}
 }
