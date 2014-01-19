781349c1a5c464a70c5b313ae9a34d4ce58a85b8
drg <drg@172.17.149.212>
2014-01-19 14:59:54 +0800

    recommend

--- src/com/teami/watertip/RecommendActivity.java
+++ src/com/teami/watertip/RecommendActivity.java
@@ -2,6 +2,10 @@ package com.teami.watertip;
 
 import java.util.ArrayList;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import com.teami.watertip.R.drawable;
+
 import android.app.Activity;
 import android.content.res.XmlResourceParser;
 import android.graphics.drawable.Drawable;
@@ -11,8 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RecommendActivity extends Activity {
-	private int count;
-	
+	private static int count;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,45 +32,56 @@ public class RecommendActivity extends Activity {
 //		image.setImageResource(R.drawable.recommend.°ÙºÏ»¨);
 		XmlResourceParser allScores=getResources().getXml(R.xml.recommend);
 //		XmlResourceParser mockFriendScores=getResources().getXml(R.xml.recommend);
-		int eventType=-1;
-		boolean bFoundScores=false;
+		//int eventType=-1;
 		ArrayList<Recommend> recommends=new ArrayList<Recommend>();
+		int eventType=-1;
+//		try {
+//			while(eventType!=XmlResourceParser.END_DOCUMENT){
+//				if(eventType==XmlResourceParser.START_TAG){
+//					String strname=allScores.getName();
+//					if(strname.equals("score")){
+//						String str_title=allScores.getAttributeValue(null,"title");
+//						String str_image=allScores.getAttributeValue(null,"image");
+//						String str_detail=allScores.getAttributeValue(null,"detail");
+//						
+//						recommends.add(new Recommend(str_title, str_image, str_detail));			
+//					}
+//				}
+//				allScores.next();
+//			}
+//		} catch (XmlPullParserException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		}
+//				
+//		title.setText(recommends.get(0).getTitle());
+//		image.setImageDrawable(getResources().getDrawable(R.drawable.clear));
+//		detail.setText(recommends.get(0).getDetail());
 		
-		while(eventType!=XmlResourceParser.END_DOCUMENT){
-			if(eventType==XmlResourceParser.START_DOCUMENT){
-				String strname=allScores.getName();
-				if(strname.equals("score")){
-					String str_title=allScores.getAttributeValue(0);
-					String str_image=allScores.getAttributeName(1);
-					String str_detail=allScores.getAttributeName(2);
-					Recommend recommend=new Recommend(str_title, str_image, str_detail);
-					recommends.add(recommend);
-					
-				}
-			}
-		}
-		title.setText(recommends.get(0).getTitle());
-		detail.setText(recommends.get(0).getDetail());
+		
 	}
 	
 	class Recommend{
-		private String title;
-		private String image;
-		private String detail;
+		private String r_title;
+		private String r_image;
+		private String r_detail;
 		
 		public Recommend(String title,String image,String detail){
-			this.title=title;
-			this.image=image;
-			this.detail=detail;			
+			this.r_title=title;
+			this.r_image=image;
+			this.r_detail=detail;			
 		}
 		public String getTitle(){
-			return title;
+			return r_title;
 		}
 		public String getImage(){
-			return image;
+			return r_image;
 		}
 		public String getDetail(){
-			return detail;
+			return r_detail;
 		}
 	}
 	
